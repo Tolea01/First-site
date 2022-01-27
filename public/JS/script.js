60abc6ad985286ef783de7515446ea8e964e440d
Tolea2001 <anatoliecurbat@gmail.com>
2022-01-27 23:46:20 +0300

    id

--- public/JS/script.js
+++ public/JS/script.js
@@ -2,6 +2,8 @@ var form = document.getElementById('form-button');
 form.onclick = function(event){
     event.preventDefault();
     var inputs = document.getElementsByTagName('input');
+
+
     for(var i = 0; i < inputs.length; i++){
         if(inputs[i].value == ""){
             inputs[i].classList.add('input-error');
@@ -10,4 +12,7 @@ form.onclick = function(event){
             inputs[i].classList.remove('input-error');
         }
     }
+
+
+    
 }
